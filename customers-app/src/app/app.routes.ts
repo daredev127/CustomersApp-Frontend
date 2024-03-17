d9690950af86d55d31a738bc61a9321a71c57a75
francis <francis@DESKTOP-OE02C04>
2024-03-17 11:37:05 +1100

    added customer list component

--- customers-app/src/app/app.routes.ts
+++ customers-app/src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { CustomerListComponent } from './features/customer/customer-list/customer-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: CustomerListComponent }];
